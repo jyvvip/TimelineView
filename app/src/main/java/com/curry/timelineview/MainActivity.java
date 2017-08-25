8df72e5341fbe45caf58c5838b530b54363f47d8
ithinker <jyvvip@163.com>
2017-08-25 23:56:10 +0800

    first commit

---
+++ app/src/main/java/com/curry/timelineview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.curry.timelineview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
